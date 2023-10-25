6f9f4ea4115d6f5e0f13a2c04730227da37b9b04
sam <semkamirov@gmail.com>
2023-10-25 20:58:35 +0500

    Отрисуй меня полностью

--- js/render.js
+++ js/render.js
@@ -1,27 +1,27 @@
 import {generateDescriptions} from './posts';
 
-var pictureTemplate = document.querySelector('#picture').content
-var pictureText = pictureTemplate.children
-var picturesContainer = document.querySelector('.pictures')
-var picturesFragment = picturesContainer.createDocumentFragment
+const pictureTemplate = document.querySelector('#picture').content;
+const picturesContainer = document.querySelector('.pictures');
 
 const picturesDescriptions = generateDescriptions();
 
 const createPicture = (pictureData) => {
-  const pictureElement = pictureTemplate.cloneNode(true)
+  const pictureElement = pictureTemplate.cloneNode(true);
 
-  let pictureImg = pictureElement.querySelector('a img')
-  let pictureLikes = pictureElement.querySelector('a .picture__likes')
-  let pictureComments = pictureElement.querySelector('a .picture__comments')
+  const pictureImg = pictureElement.querySelector('a img');
+  const pictureLikes = pictureElement.querySelector('a .picture__likes');
+  const pictureComments = pictureElement.querySelector('a .picture__comments');
 
-  pictureImg.src = pictureData.url
-  pictureImg.alt = pictureData.description
-  pictureLikes.textContent = pictureData.likes
+  // pictureElement.querySelector('a img').src = pictureData.url
+
+  pictureImg.src = pictureData.url;
+  pictureImg.alt = pictureData.description;
+  pictureLikes.textContent = pictureData.likes;
   pictureComments.textContent = pictureData.comments.length;
 
-  picturesContainer.appendChild(pictureElement)
+  picturesContainer.appendChild(pictureElement);
 };
 
-picturesDescriptions.forEach(picture => {
-  createPicture(picture)
+picturesDescriptions.forEach((picture) => {
+  createPicture(picture);
 });
