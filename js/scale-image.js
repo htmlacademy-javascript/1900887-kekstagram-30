a0a74b4fa7258f4f419dc3f50a123c59eafca639
sam <semkamirov@gmail.com>
2023-11-09 20:03:41 +0500

    Валидирует форму

--- js/scale-image.js
+++
@@ -1,7 +0,0 @@
-const scaleControlSmaller = document.querySelector('.scale__control--smaller');
-const scaleControlBigger = document.querySelector('.scale__control--bigger');
-const scaleControlValue = document.querySelector('.scale__control--value');
-const SCALE_STEP = 25;
-
-
-
