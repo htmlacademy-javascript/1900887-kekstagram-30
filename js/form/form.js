f34d96d60a8afbb32ba9d03932c95bd9a518ecdc
sam <semkamirov@gmail.com>
2023-11-19 12:12:07 +0500

    Получает данные с сервера

--- js/form/form.js
+++ js/form/form.js
@@ -2,6 +2,7 @@ import {isEscapeKey} from '../utils/utils.js';
 import {addValidators, validateForm, resetPristine} from './validate-form.js';
 import {scalePreview} from './scale-preview.js';
 import {initSlider, resetSlider} from './add-effect.js';
+import {uploadFormData} from './upload-form.js';
 // import {setEffects, resetSlider} from './add-effect.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
@@ -9,8 +10,13 @@ const uploadFormInput = document.querySelector('.img-upload__input');
 const uploadFormEdit = document.querySelector('.img-upload__overlay');
 const imgUploadCancelBtn = document.querySelector('.img-upload__cancel');
 
+const successTemplate = document.querySelector('#success').content;
+const successMessage = successTemplate.querySelector('.success');
+const errorTemplate = document.querySelector('#error').content;
+const errorMessage = errorTemplate.querySelector('.error');
+
 const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt) && !evt.target.closest('.text__hashtags') && !evt.target.closest('.text__description')) {
+  if (isEscapeKey(evt) && !evt.target.closest('.text__hashtags') && !evt.target.closest('.text__description') && !document.body.contains(successMessage) && !document.body.contains(errorMessage)) {
     evt.preventDefault();
     closeForm();
   }
@@ -18,11 +24,13 @@ const onDocumentKeydown = (evt) => {
 const onUploadCancelClick = () => {
   closeForm();
 };
+
 const onFormSubmit = (evt) => {
   evt.preventDefault();
   const isValid = validateForm();
   if (isValid) {
-    // const formData = new FormData(evt.target);
+    const formData = new FormData(evt.target);
+    uploadFormData(formData);
   }
 };
 function closeForm() {
@@ -31,7 +39,6 @@ function closeForm() {
   document.removeEventListener('keydown', onDocumentKeydown);
   resetPristine();
   resetSlider();
-  // resetSlider();
 }
 function openForm() {
   uploadFormEdit.classList.remove('hidden');
@@ -41,14 +48,13 @@ function openForm() {
   uploadForm.addEventListener('submit', onFormSubmit);
 }
 const handleInputChange = () => {
-  addValidators();
-  initSlider();
-  // setEffects();
-  scalePreview();
   openForm();
 };
 const uploadImage = () => {
+  initSlider();
+  addValidators();
+  scalePreview();
   uploadFormInput.addEventListener('change', handleInputChange);
 };
 
-export {uploadImage};
+export {uploadImage, closeForm};
--- js/form/upload-form.js
+++ js/form/upload-form.js
@@ -1,10 +1,72 @@
-// const SERVER_URL = 'https://30.javascript.pages.academy/kekstagram';
-// const uploadFormData = (formData) => fetch(
-//   SERVER_URL,
-//   {
-//     method: 'POST',
-//     headers: {'Content-Type': 'multipart/form-data'},
-//     body: JSON.stringify(formData)
-//   });
-//
-// export {uploadFormData};
+const SERVER_URL = 'https://30.javascript.pages.academy/kekstagram';
+
+import {isEscapeKey} from '../utils/utils.js';
+
+const successTemplate = document.querySelector('#success').content;
+const successMessage = successTemplate.querySelector('.success');
+const errorTemplate = document.querySelector('#error').content;
+const errorMessage = errorTemplate.querySelector('.error');
+const errorButton = errorTemplate.querySelector('.error__button');
+const successButton = successTemplate.querySelector('.success__button');
+
+const isSuccess = () => document.body.contains(successMessage);
+const isError = () => document.body.contains(errorMessage);
+
+const onWindowClick = (evt) => {
+  if ((!successMessage.contains(evt.target) || !errorMessage.contains(evt.target)) && (isSuccess() || isError())) {
+    closeMessageWin();
+  }
+};
+
+const onDocumentKeyDown = (evt) => {
+  if (isEscapeKey(evt) && (isSuccess() || isError())) {
+    evt.preventDefault();
+    closeMessageWin();
+  }
+};
+
+const onErrorBtnClick = () => {
+  document.body.removeChild(errorMessage);
+};
+
+const onSuccessBtnClick = () => {
+  document.body.removeChild(successMessage);
+};
+
+function closeMessageWin() {
+  errorButton.removeEventListener('click', onErrorBtnClick);
+  document.removeEventListener('keydown', onDocumentKeyDown);
+  window.removeEventListener('click', onWindowClick);
+
+  if (isSuccess()) {
+    document.body.removeChild(successMessage);
+  } else {
+    document.body.removeChild(errorMessage);
+  }
+}
+
+const onUploadSuccess = (response) => {
+  if (response.ok) {
+    document.body.insertAdjacentElement('beforeend', successMessage);
+    successButton.addEventListener('click', onSuccessBtnClick);
+    document.addEventListener('keydown', onDocumentKeyDown);
+    window.addEventListener('click', onWindowClick);
+  }
+};
+
+const onUploadError = () => {
+  document.body.insertAdjacentElement('beforeend', errorMessage);
+  errorButton.addEventListener('click', onErrorBtnClick);
+  document.addEventListener('keydown', onDocumentKeyDown);
+  window.addEventListener('click', onWindowClick);
+};
+const uploadFormData = (formData) => fetch(
+  SERVER_URL,
+  {
+    method: 'POST',
+    body: formData
+  })
+  .then((response) => onUploadSuccess(response))
+  .catch(() => onUploadError());
+
+export {uploadFormData};
